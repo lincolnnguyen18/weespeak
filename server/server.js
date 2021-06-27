1b38ba068e0ab641c3e9646bbb6db572831232bd
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2021-06-27 06:57:28 -0500

    reformat spaces to tabs server.js

--- server/server.js
+++ server/server.js
@@ -8,18 +8,18 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 // An api endpoint that returns a short list of items
 app.get('/login', (req,res) => {
-    setTimeout(() => {
-        res.send('do google auth here')
-    }, 3000);
+	setTimeout(() => {
+		res.send('do google auth here')
+	}, 3000);
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/../client/build/index.html'));
+	res.sendFile(path.join(__dirname+'/../client/build/index.html'));
 });
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/../client/build/index.html'));
+	res.sendFile(path.join(__dirname+'/../client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
