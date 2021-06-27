745f101e222a8f3120d4be8885be374f27ddd4ae
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2021-06-27 07:01:40 -0500

    clean up express routing

--- server/server.js
+++ server/server.js
@@ -4,17 +4,13 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 // An api endpoint that returns a short list of items
 app.get('/login', (req,res) => {
 	setTimeout(() => {
 		res.send('do google auth here')
 	}, 3000);
-});
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname+'/../client/build/index.html'));
 });
 
 // Handles any requests that don't match the ones above
