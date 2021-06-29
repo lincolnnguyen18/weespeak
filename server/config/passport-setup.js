bb09ad672d152c2af2120a54b6b618a66cdea370
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2021-06-29 09:04:02 -0500

    remembering users

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -11,19 +11,25 @@ passport.use(
 		clientSecret: keys.google.clientSecret
 	}, (accessToken, refreshToken, profile, done) => {
 		// passport callback function
-		console.log(profile);
-		console.log(profile._json);
-		console.log(profile._json.email_verified);
-		verified = profile._json.email_verified;
-		new User({
-			username: profile._json.name,
-			googleId: profile._json.sub,
-			email: profile._json.email,
-			emailVerified: profile._json.email_verified,
-			picture: profile._json.picture,
-			locale: profile._json.locale
-		}).save().then((newUser) => {
-			console.log('new user created: ' + newUser);
-		});
+		// check if user already exists in db
+		User.findOne({googleId: profile._json.sub}).then((currentUser) => {
+			if (currentUser) {
+				// already have user
+				console.log('user is: ', currentUser);
+			} else {
+				// if not, create user in db
+				console.log(profile);
+				new User({
+					username: profile._json.name,
+					googleId: profile._json.sub,
+					email: profile._json.email,
+					emailVerified: profile._json.email_verified,
+					picture: profile._json.picture,
+					locale: profile._json.locale
+				}).save().then((newUser) => {
+					console.log('new user created: ' + newUser);
+				});
+			}
+		})
 	})
 )
