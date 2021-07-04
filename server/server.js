deba4b0a08810f2676326742d3f272ad9d523b6e
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2021-07-04 18:18:29 -0500

    react routing works again

--- server/server.js
+++ server/server.js
@@ -35,9 +35,9 @@ app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
 
 // Handles any requests that don't match the ones above
-// app.get('*', (req,res) =>{
-// 	res.sendFile(path.join(__dirname+'/../client/build/index.html'));
-// });
+app.get('*', (req,res) =>{
+	res.sendFile(path.join(__dirname+'/../client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
