fa52712a1c2719ed2bbb91fa15c81d7a3373072e
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2021-07-15 16:52:41 -0500

    removed garbage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,6 @@
 import React, { useEffect } from 'react'
 
 export default function Home() {
-	// useEffect(() => {
-	// 	fetch('http://localhost:5000/profile/isSignedIn')
-	// 	.then(response => response.json())
-	// 	.then(data => {
-	// 		if (data["signedIn"] != "true") {
-	// 			window.location.href = '/'
-	// 		}
-	// 	});
-	// }, []);
-
 	return (
 		<div>
 			Home
