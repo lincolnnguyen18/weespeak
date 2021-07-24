829cdd136c10f34afc0898ef99d9e2d8a8198649
lincolnnguyen18 <letsmakeaworld@icloud.com>
2021-07-23 22:39:32 -0500

    coturn pass

--- demos/chat-demo/start.sh
+++ demos/chat-demo/start.sh
@@ -1,6 +1,11 @@
 # LINCOLNNGUYEN18.COM
 # TURN/STUN
 
+if screen -list | grep -q "lincolnnguyen18-turn-stun-coturn"; then
+    echo "Server already running"
+    exit 1
+fi
+
 # Start coturn server
 screen -dmS lincolnnguyen18-turn-stun-coturn
-screen -S lincolnnguyen18-turn-stun-coturn -X stuff "sudo turnserver -a -f -r -X 107.196.10.160/192.168.1.69 lincolnnguyen18.com\n"
+screen -S lincolnnguyen18-turn-stun-coturn -X stuff "sudo turnserver -a -f -r -X 107.196.10.160/192.168.1.69 lincolnnguyen18.com\n$my_pass\n"
