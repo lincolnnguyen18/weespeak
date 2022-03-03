add54d8adef8ef9ca96fe60efe2459ba296f7914
lincolnnguyen18 <letsmakeaworld@icloud.com>
2022-03-03 15:47:40 -0600

    update

--- demos/chat-demo/kill.sh
+++ demos/chat-demo/kill.sh
@@ -1,19 +1,30 @@
-# LINCOLNNGUYEN18.COM
-# TURN/STUN
+# # LINCOLNNGUYEN18.COM
+# # TURN/STUN
 
-# Kill coturn server
-sudo kill $(sudo lsof -t -i:3478) &>/dev/null
-sudo kill $(sudo lsof -t -i:3479) &>/dev/null
-sudo kill $(sudo lsof -t -i:5349) &>/dev/null
-sudo kill $(sudo lsof -t -i:5350) &>/dev/null
-sudo kill $(sudo lsof -t -i:7002) &>/dev/null
-screen -S l18-coturn -X quit
-screen -S l18-weespeak -X quit
+# # Kill coturn server
+# sudo kill $(sudo lsof -t -i:3478) &>/dev/null
+# sudo kill $(sudo lsof -t -i:3479) &>/dev/null
+# sudo kill $(sudo lsof -t -i:5349) &>/dev/null
+# sudo kill $(sudo lsof -t -i:5350) &>/dev/null
+# sudo kill $(sudo lsof -t -i:7002) &>/dev/null
+# screen -S l18-coturn -X quit
+# screen -S l18-weespeak -X quit
 
-# Confirm killed
-echo "Checking if coturn is still alive..."
-sudo lsof -i :3478
-sudo lsof -i :3479
-sudo lsof -i :5349
-sudo lsof -i :5350
-sudo lsof -i :7002
+# # Confirm killed
+# echo "Checking if coturn is still alive..."
+# sudo lsof -i :3478
+# sudo lsof -i :3479
+# sudo lsof -i :5349
+# sudo lsof -i :5350
+# sudo lsof -i :7002
+
+if ! screen -list | grep -q 'd2'; then
+  echo 'd2 already killed'
+  exit 1
+else
+  echo "killing d2"
+fi
+. /media/sda1/deployment/ports.sh
+screen -S 'd2' -X quit
+echo "Checking if d2 still alive..."
+lsof -i:$d2
--- demos/chat-demo/start.sh
+++ demos/chat-demo/start.sh
@@ -1,13 +1,27 @@
-# LINCOLNNGUYEN18.COM
-# TURN/STUN
+# # LINCOLNNGUYEN18.COM
+# # TURN/STUN
 
-if screen -list | grep -q "l18-coturn"; then
-    echo "Server already running"
-    exit 1
+# if screen -list | grep -q "l18-coturn"; then
+#     echo "Server already running"
+#     exit 1
+# fi
+
+# # Start coturn server
+# screen -dmS l18-coturn
+# screen -S l18-coturn -X stuff "sudo turnserver -a -f -r -X 107.196.10.160/192.168.1.69 lincolnnguyen18.com\n$my_pass\n"
+# screen -dmS l18-weespeak
+# screen -S l18-weespeak -X stuff "node index2.js\n"
+
+
+if screen -list | grep -q 'd2'; then
+  echo 'd2 already started'
+  exit 1
+else
+  echo "starting d2"
 fi
-
-# Start coturn server
-screen -dmS l18-coturn
-screen -S l18-coturn -X stuff "sudo turnserver -a -f -r -X 107.196.10.160/192.168.1.69 lincolnnguyen18.com\n$my_pass\n"
-screen -dmS l18-weespeak
-screen -S l18-weespeak -X stuff "node index2.js\n"
+. /media/sda1/deployment/ports.sh
+screen -dmS 'd2'
+screen -S 'd2' -X stuff 'source /media/sda1/deployment/ports.sh\n'
+screen -S 'd2' -X stuff '(node index2.js)\n'
+echo "Checking if d2 started..."
+lsof -i:$d2
